a96a46780ca9ea4c7408402b26d38ff898b9df92
zoutenghao <tenghao.zou@xgo.one>
2025-08-29 14:16:38 +0800

    refactor: comment out unused notification functions in usePany and App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
 import { useGreyAndColorWeakness } from "@@/composables/useGreyAndColorWeakness"
-import { usePany } from "@@/composables/usePany"
+// import { usePany } from "@@/composables/usePany"
 import { useTheme } from "@@/composables/useTheme"
 import zhCn from "element-plus/es/locale/lang/zh-cn" // Element Plus 中文包
 
@@ -8,7 +8,7 @@ const { initTheme } = useTheme()
 
 const { initGreyAndColorWeakness } = useGreyAndColorWeakness()
 
-const { initStarNotification, initStoreNotification, initMobileNotification } = usePany()
+// const { initStarNotification, initStoreNotification, initMobileNotification } = usePany()
 
 // 初始化主题
 initTheme()
@@ -16,12 +16,12 @@ initTheme()
 // 初始化灰色模式和色弱模式
 initGreyAndColorWeakness()
 
-// #region 初始化通知
-initStarNotification()
+// // #region 初始化通知
+// initStarNotification()
 
-initStoreNotification()
+// initStoreNotification()
 
-initMobileNotification()
+// initMobileNotification()
 // #endregion
 </script>
 
